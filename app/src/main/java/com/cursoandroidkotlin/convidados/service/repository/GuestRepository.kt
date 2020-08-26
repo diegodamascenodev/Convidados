0ee66772a738c945cce709e6a9c922a627e46148
diegodamascenodev <diegodamasceno.dev@gmail.com>
2020-08-26 14:55:22 -0300

    - Implementação da "fun update()", que atualiza dados no banco de dados - Fim da aula 153

--- app/src/main/java/com/cursoandroidkotlin/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/com/cursoandroidkotlin/convidados/service/repository/GuestRepository.kt
@@ -36,8 +36,21 @@ class GuestRepository private constructor(context: Context){
         }
     }
 
-    fun update(guest: GuestModel) {
+    fun update(guest: GuestModel) : Boolean {
+        try{
+            val db = mGuestDataBaseHelper.writableDatabase
 
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"                             //Quem é o ponto de interrogação? É o valor da variavel "args", definida na linha de baixo
+            val args = arrayOf(guest.id.toString())
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, contentValues, selection, args)
+            return true
+        } catch (e: Exception) {
+            return false
+        }
     }
 
     fun delete(guest: GuestModel) {
