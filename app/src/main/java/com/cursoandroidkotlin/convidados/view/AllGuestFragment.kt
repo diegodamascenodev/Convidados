9b54c9bf0f90c4e27e18aa92005a5a95008eee0a
diegodamascenodev <diegodamasceno.dev@gmail.com>
2020-08-27 15:49:11 -0300

    - Arquivos de layout renomeados:      * AbsentFragment.kt      * AllGuestFragment.kt      * PresentFragment.kt - Criação de pacotes e arquivos pertencentes a estrutura de listagem que será utilizada na 'fun getAll()':      * adapter           > GuestAdapter.kt      * viewholder           > GuestViewHolder.kt - Fim da aula 159

--- app/src/main/java/com/cursoandroidkotlin/convidados/view/AllGuestFragment.kt
+++ app/src/main/java/com/cursoandroidkotlin/convidados/view/AllGuestFragment.kt
@@ -7,25 +7,34 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cursoandroidkotlin.convidados.R
+import com.cursoandroidkotlin.convidados.view.adapter.GuestAdapter
 import com.cursoandroidkotlin.convidados.viewmodel.AllGuestViewModel
+import kotlinx.android.synthetic.main.fragment_all.*
 
 class AllGuestFragment : Fragment() {
 
     private lateinit var allGuestViewModel: AllGuestViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        allGuestViewModel = ViewModelProviders.of(this).get(AllGuestViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        allGuestViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        allGuestViewModel = ViewModelProvider(this).get(AllGuestViewModel::class.java)
+
+        val root = inflater.inflate(R.layout.fragment_all, container, false)
+
+        //RecyclerView
+
+        // Passo 1 - Obter o elemento RecyclerView no XML
+        val recycler = root.findViewById<RecyclerView>(R.id.recycler_all_guests)
+
+        // Passo 2 - Definir um layout para o Recycler
+        recycler.layoutManager = LinearLayoutManager(context)
+
+        // Passo 3 - Definir um adapter
+        recycler.adapter = GuestAdapter()
+
         return root
     }
 }
